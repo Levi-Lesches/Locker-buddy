1d4805962d56ab59a8819efb59c664ca8dae02ed
L Lesches <levilesches@gmail.com>
2019-03-18 12:43:30 -0400

    Light loops

--- tech.ino
+++ tech.ino
@@ -1,22 +1,24 @@
 #define NUM_LED 2
 
-const int led[NUM_LED] = {2, 3};
+const int led[NUM_LED] = {2, 3, 6};
 int index = 0;
 
 void setup() {
-	for (int index_ = 0; index_ < NUM_LED; index_++) 
+	for (int index_ = 0; index_ < NUM_LED; index_++)
 		pinMode (led [index_], OUTPUT);
 }
 
 void loopIndex(int increment) {
-	pinMode (led[index], LOW);
+	digitalWrite (led[index], LOW);
 	index += increment;
 	if (index < 0) index = NUM_LED;
 	else if (index == NUM_LED) index = 0;
-	pinMode (led[index], HIGH);
+	digitalWrite (led[index], HIGH);
 }
 
 void loop() {
 	loopIndex(1);
 	delay (1000);
 }
+
+// void loop(){}
